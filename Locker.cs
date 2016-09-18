e8f0945bc9ba4c39268719d1f95e288a738ce608
Sean Fisher <seandavidfisher@gmail.com>
2016-09-18 16:22:14 -0500

    Added license, changed spaces to tabs

--- Locker.cs
+++ Locker.cs
@@ -1,35 +1,35 @@
 namespace LockerRiddle
 {
-    public class Locker
-    {
-        public bool Open { get; set; }
+	public class Locker
+	{
+		public bool Open { get; set; }
 
-        public uint TimesOpened { get; set; }
-        public uint TimesClosed { get; set; }
-        public double PercentOpen => (double)TimesOpened / (double)(TimesOpened + TimesClosed) * 100.0;
+		public uint TimesOpened { get; set; }
+		public uint TimesClosed { get; set; }
+		public double PercentOpen => (double)TimesOpened / (double)(TimesOpened + TimesClosed) * 100.0;
 
-        public string GetState()
-        {
-            return Open ? "Open" : "Closed";
-        }
+		public string GetState()
+		{
+			return Open ? "Open" : "Closed";
+		}
 
-        public void Toggle()
-        {
-            if (Open)
-            {
-                TimesClosed++;
-            }
-            else
-            {
-                TimesOpened++;
-            }
+		public void Toggle()
+		{
+			if (Open)
+			{
+				TimesClosed++;
+			}
+			else
+			{
+				TimesOpened++;
+			}
 
-            Open = !Open;
-        }
+			Open = !Open;
+		}
 
-        public override string ToString()
-        {
-            return $"State: {GetState()}\tTimesOpened: {TimesOpened}\tTimesClosed: {TimesClosed}\tPercentOpen: {PercentOpen}%";
-        }
-    }
+		public override string ToString()
+		{
+			return $"State: {GetState()}\tTimesOpened: {TimesOpened}\tTimesClosed: {TimesClosed}\tPercentOpen: {PercentOpen}%";
+		}
+	}
 }
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # The Locker Riddle
 
-Taken from https://www.youtube.com/watch?v=c18GjbnZXMw
+This simple .NET Core code was written to solve the Locker Riddle as explained on the TED-Ed YouTube video called `Can you solve the locker riddle? - Lisa Winer`.
+
+[![Can you solve the locker riddle? - Lisa Winer](https://img.youtube.com/vi/c18GjbnZXMw/0.jpg)](https://www.youtube.com/watch?v=c18GjbnZXMw)
+
+Feel free to suggest improvements to the algorithm.
